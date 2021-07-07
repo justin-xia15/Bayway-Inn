69c6c222058f584c687698758b714c5bc625c7b2
justin-xia15 <83360815+justin-xia15@users.noreply.github.com>
2021-07-07 18:24:33 -0400

    Added button animation

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function hamburgerAnimation(x) {
-  x.classList.toggle("change");
-}
-
 $(".hamburger-btn").click(function() {
   $(".menu").slideToggle();
 });
+
+$(".btn").click(function() {
+  $(".btn").addClass("pressed");
+  setTimeout(function() {
+    $(".btn").removeClass("pressed");
+  }, 100)
+});
+
+function hamburgerAnimation(x) {
+  x.classList.toggle("change");
+}
